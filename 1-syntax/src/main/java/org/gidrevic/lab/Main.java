f57fbd45e93ce26a03fa5913a3779c193f093957
AlexCawl <babushkinsusu@gmail.com>
2024-05-05 23:38:36 +0500

    feat: lab1 done

--- 1-syntax/src/main/java/org/gidrevic/lab/Main.java
+++ 1-syntax/src/main/java/org/gidrevic/lab/Main.java
@@ -1,17 +1,54 @@
 package org.gidrevic.lab;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Random random = new Random();
+
+    private static int getRandom(int min, int max) {
+        return random.nextInt(max - min) + min;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Define length of an array:");
+
+        int length = input.nextInt();
+        System.out.println("Define bounds of an array values:");
+
+        int min = input.nextInt();
+        int max = input.nextInt();
+
+        if (max < min) {
+            throw new IllegalArgumentException("Invalid bounds for values!");
         }
+
+        int [] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getRandom(min, max);
+        }
+
+        System.out.println("Filled array:");
+        for (int number : array) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("Swapped array:");
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (i == 0 && array.length > 1) {
+                value = array[i + 1];
+            } else if (i == array.length - 1 && array.length > 1) {
+                value = array[i - 1];
+            } else if (array.length > 2) {
+                value = (array[i - 1] + array[i] + array[i + 1]) / 3;
+            }
+            array[i] = value;
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
