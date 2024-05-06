7c9b2270a47fbdcfe1fa270c6eec1e1223849ede
AlexCawl <babushkinsusu@gmail.com>
2024-05-06 23:48:31 +0500

    feat: dev lab7

---
+++ 7-collections/src/main/java/org/gidrevic/lab/schedule/Schedule.java
@@ -0,0 +1,8 @@
+package org.gidrevic.lab.schedule;
+
+import org.gidrevic.lab.util.Indexable;
+
+public class Schedule {
+    private final Integer id;
+    private final String
+}
---
+++ 7-collections/src/main/java/org/gidrevic/lab/util/NotFoundException.java
@@ -0,0 +1,4 @@
+package org.gidrevic.lab.util;
+
+public class NotFoundException extends RuntimeException {
+}
